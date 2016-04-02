a6be90471ce2228ec0ef7ebe531ec97711f3464c
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-04-02 11:35:16 -0600

    wip

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: 'postgresql://localhost/taco-valet'
+    connection: 'postgresql://localhost/taco-valet' || process.env.DATABASE_URL
   }
 
 };
