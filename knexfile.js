98afce020af3c1cebb59dcb290de9770d028d581
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-04-04 11:38:17 -0600

    ready for heroku

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,22 @@
+require('dotenv').load();
+
 module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: 'postgresql://localhost/taco-valet' || process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    }
   }
 
 };
